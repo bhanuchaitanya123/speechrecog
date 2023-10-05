abb6cf3755b5960ffc4c2a26fcedcf7c28aa3f2f
bhanuchaitanya123 <142810475+bhanuchaitanya123@users.noreply.github.com>
2023-10-05 19:28:31 +0530

    Update auto_text_reader.py

--- auto_text_reader.py
+++ auto_text_reader.py
@@ -1,4 +1,3 @@
-
 import googletrans
 import googletrans
 from tkinter import *
@@ -40,6 +39,43 @@ def b():
     os.system("start audpy.mp3")
 t.onkey(b, 'b')
 t.listen()
-
-
-
+def c():
+  import speech_recognition as sr
+  import pyttsx3 as p
+  import wikipedia as w
+  import pywhatkit 
+  #pywhatkit.sendwhatmsg("+91", "Hello", 22, 28)
+  #pywhatkit.playonyt("GeeksforGeeks")
+  #pywhatkit.search("GeeksforGeeks")
+  #pywhatkit.sendwhats_image("+91, "Images/Hello.png")
+  engine=p.init()
+  voices=engine.getProperty('voices')
+  engine.setProperty('voice',voices[1].id)
+  r=sr.Recognizer()
+  with sr.Microphone() as s:
+          aud=r.listen(s)
+          text=r.recognize_google(aud)
+          text=text.lower()
+          if 'alexa' in text:
+              text=text.replace('alexa','')
+  print(text)
+  if 'play' in text:
+      song=text.replace('play','')
+      engine.say(song)
+      engine.runAndWait()
+      pywhatkit.playonyt(song)
+  elif 'search' or 'open' in text:
+   if 'search' in text:
+      text=text.replace('search','') 
+      pywhatkit.search(text)
+   if 'open' in text:
+       text=text.replace('open','')
+       pywhatkit.search(text)
+  elif 'who' in text:
+      person=text.replace('who','')
+      info=w.summary(person,1)
+      engine.say(info)
+      engine.runAndWait()
+      
+t.onkey(c, 'c')
+t.listen()
